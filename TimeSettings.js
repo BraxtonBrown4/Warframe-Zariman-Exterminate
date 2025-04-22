b800063a175dfb2aeebca74839c09e01ad8c4694
Braxton Brown <braxtoncarterbrown@gmail.com>
2025-04-22 08:56:23 -0500

    changed time to check for centrel time

--- TimeSettings.js
+++ TimeSettings.js
@@ -1,9 +1,21 @@
 import { WFMessage } from "./MessageFunc.js"
 
-const currentTime = new Date().getHours()
+const now = new Date();
+
+const options = {
+  timeZone: "America/Chicago",
+  hour: "numeric",
+  hour12: false,
+};
+
+const formatter = new Intl.DateTimeFormat("en-US", options);
+const currentTime = parseInt(formatter.format(now));
+
+console.log(currentTime)
+
 
 export const timeSettings = (message) => {
-    if (currentTime >= 15 || currentTime <= 4) {
+    if (currentTime >= 10 && currentTime <= 22) {
             WFMessage(message)
     } else {
         console.log('Warframe Zariman Exterminate is not messaging right now due to nighttime restrictions.')
